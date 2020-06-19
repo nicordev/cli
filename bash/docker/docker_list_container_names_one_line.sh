e9717d7cf5b164eb8f96c587e0573377030ea018
Nicolas Renvoisé <n.renvoise@ubitransport.com>
2020-06-19 11:11:11 +0200

    feat: add docker list container names

---
+++ bash/docker/docker_list_container_names_one_line.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+## List docker container names.
+##
+## The 1st parameter can be used to filter results using grep.
+
+getContainerNames() {
+    docker inspect --format='{{.Name}}' $(sudo docker ps -aq --no-trunc)
+}
+
+containers=$(getContainerNames)
+
+if [ ! -z "$1" ]; then
+    echo $containers | sed 's#/##g' | sed 's# #\n#g' | grep "$1" | tr '\n' ' '
+    exit
+fi
+
+echo $containers | sed 's#/##g'
