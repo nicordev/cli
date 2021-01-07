bb4d2f1d3e8fb0d97d41dd647e133cbf1b5f1de9
Nicolas Renvoisé <n.renvoise@ubitransport.com>
2021-01-07 12:01:35 +0100

    improve create_please_script

--- script/bash/create_script/create_please_script.sh
+++ script/bash/create_script/create_please_script.sh
@@ -18,7 +18,7 @@ writeScriptTemplate() {
 SCRIPT_NAME=\$(basename \$0)
 
 functionName() {
-    echo "Welcome home!"
+    echo -e "\e[32m Welcome home! \e[0m \n You are executing: \e[33m \${FUNCNAME[0]} \e[0m with \$# parameter(s)"
 }
 
 _listAvailableFunctions() {
@@ -35,5 +35,13 @@ END
 }
 
 fileName="please_${suffix}.sh"
+
+if [ -f "$fileName" ]; then
+    echo -e "file \e[33m$fileName\e[0m already exists.\n\nPlease remove it first:\n\e[33mrm $fileName\e[0m"
+    exit 1
+fi
+
+echo "Creating script ${fileName}..."
 createExecutableFile "$fileName"
 writeScriptTemplate "$fileName"
+echo -e "Done. \e[32mEnjoy!\e[0m"
